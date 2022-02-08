d3aac96ebe8fb2c9a84e2c1099a1726363ede704
kushmay <81627608+kushmay@users.noreply.github.com>
2022-02-08 17:47:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+Webcam.set({
+width:250,
+height:250,
+image_format:'png',
+png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>'
+    });
+}
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/i_m5Erh8u/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('Model Loaded!');
+}
+var prediction_1 , prediction_2;
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data_1="The first prediction is " + prediction_1;
+    speak_data_2="And the second prediction is " + prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    utterThis.rate=0.5;
+    synth.speak(utterThis);
+
+}
+
+function check()
+{
+img=document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML=results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label =="best")
+    {
+        document.getElementById("update_gesture").innerHTML="&#128077;";
+    }
+    if(results[0].label =="rock")
+    {
+        document.getElementById("update_gesture").innerHTML="&#9994;";
+    }
+    if(results[0].label =="click")
+    {
+        document.getElementById("update_gesture").innerHTML="&#128070;";
+    }
+
+    if(results[1].label =="best")
+    {
+        document.getElementById("update_gesture1").innerHTML="&#128077;";
+    }
+    if(results[1].label =="rock")
+    {
+        document.getElementById("update_gesture1").innerHTML="&#79994;";
+    }
+    if(results[1].label =="click")
+    {
+        document.getElementById("update_gesture1").innerHTML="&#128070;";
+    }
+    }
+}
